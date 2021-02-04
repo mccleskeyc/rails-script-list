a99d7bf596b6f8b465a5b0834064cf50deafe23a
mccleskeyc <mccleskeyc@gmail.com>
2021-02-04 12:50:22 -0500

    finished new department form

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -2,15 +2,29 @@ class DepartmentsController < ApplicationController
 
     def new
         @department = Department.new
+    
         @department.scripts.build
+        @department.scripts.build
+        @department.scripts.build
+      end
+    
+      def create
+        @department = Department.new(department_params)
+    
+        if @department.save
+          redirect_to scripts_path
+        else
+          render :new
+        end
+      end
+    
+    
+      private
+    
+        def department_params
+          params.require(:department).permit(
+            :name,
+            scripts_attributes: [:title, :logline]
+          )
+        end
     end
-
-    def create
-
-    end
-
-    private
-    def department_params
-        params.require(:department).permit(:name, script_attributes: [:title, :logline])
-    end
-end
