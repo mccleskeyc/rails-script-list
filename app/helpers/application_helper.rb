7cad92832a6fa7334acb8df0b3b43d9ad94cd0b4
mccleskeyc <mccleskeyc@gmail.com>
2021-02-05 11:23:31 -0500

    DRYing code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,43 @@
 module ApplicationHelper
+
+    def render_nav_links
+        content_tag("ul") do
+            if user_signed_in? # is logged in
+                content_tag("li") do
+                    link_to("Home", root_path)
+                end +
+
+                content_tag("li") do
+                  link_to("Scripts", scripts_path)
+                end + 
+
+                content_tag("li") do
+                  link_to("Add Script", new_script_path)
+                end +
+
+                content_tag("li") do
+                    link_to("Formats", departments_path)
+                end +
+
+                content_tag("li") do
+                    link_to("Add a Format", new_department_path)
+                end +
+
+                content_tag("li") do
+                    link_to("Log Out", destroy_user_session_path)
+                end 
+            else
+                content_tag("li") do
+                    link_to("Home", root_path)
+                end +
+                content_tag("li") do
+                    link_to("Log In", new_user_session_path)
+                end + 
+                content_tag("li") do
+                    link_to("Sign Up", new_user_registration_path)
+                end
+            end
+          end
+    end
+
 end
